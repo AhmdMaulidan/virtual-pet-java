864b430428881a4baa2205a8d393146829621ff4
AhmdMaulidan <ahmd.maulidann@gmail.com>
2025-05-26 11:02:47 +0800

    first commit

---
+++ src/JFrame/start.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package JFrame;
+
+/**
+ *
+ * @author ahmd.maulidanngmail.com
+ */
+public class start {
+    public static void main(String[] args) {
+        login log = new login();
+        log.setVisible(true);
+        log.pack();
+        log.setLocationRelativeTo(null);
+        log.setDefaultCloseOperation(login.EXIT_ON_CLOSE);
+    }  
+}
